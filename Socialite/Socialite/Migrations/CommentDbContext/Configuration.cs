a8c5f940b17614ca8a4a22d802e4d42f4d8cf3ad
ValkataStz <valentins613@gmail.com>
2017-05-11 11:03:02 +0100

    Comments DB Setup

---
+++ Socialite/Socialite/Migrations/CommentDbContext/Configuration.cs
@@ -0,0 +1,32 @@
+namespace Socialite.Migrations.CommentDbContext
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Socialite.Models.CommentDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            MigrationsDirectory = @"Migrations\CommentDbContext";
+        }
+
+        protected override void Seed(Socialite.Models.CommentDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
